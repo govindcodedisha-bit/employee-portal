b4ec3f4c65ca9c954cb8d7a18cd4a0822891dee8
govindcodedisha-bit <govind.codedisha@gmail.com>
2026-03-04 06:53:19 +0530

    Changed template form as per reactive form fields

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
-import { EmployeeForm } from './pages/employee-form/employee-form';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form';
 import { ReactiveEmployeeForm } from './pages/reactive-employee-form/reactive-employee-form';
 import { EmployeeManagement } from './pages/employee-management/employee-management';
 // import { Login } from './pages/login/login';
@@ -14,7 +14,7 @@ export const appRoutes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
-  { path: 'employee-form', component: EmployeeForm },
+  { path: 'employee-form', component: EmployeeFormComponent },
   { path: 'reactive-employee-form', component: ReactiveEmployeeForm },
   { path: 'employee-management', component: EmployeeManagement },
   //{ path: 'login', component: Login },
--- src/app/pages/employee-form/employee-form.ts
+++ src/app/pages/employee-form/employee-form.ts
@@ -1,29 +1,84 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { Employee } from './employee.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee.model';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './employee-form.html',
-  styleUrl: './employee-form.css',
+  styleUrl: './employee-form.css'
 })
-export class EmployeeForm {
- employee : Employee = this.getEmptyEmployee();
+export class EmployeeFormComponent {
 
-  getEmptyEmployee(): Employee {
-    return {
-      employeeId: null,
-      employeeName: '',
-      dateOfBirth: '',
-      contactNumber: '',
-      addressLine1: ''
-    };
+  @Input() employeeData: Employee | null = null;   // 👈 incoming employee
+  @Output() save = new EventEmitter<Employee>();
+  @Output() close = new EventEmitter<void>();
+
+  isEditMode = false;
+
+  employee: any = {};
+
+  states = ['Maharashtra', 'Gujarat', 'Karnataka', 'Delhi'];
+  cities = ['Pune', 'Mumbai', 'Ahmedabad', 'Bangalore', 'Delhi'];
+
+  ngOnInit() {
+    if (this.employeeData) {
+      this.isEditMode = true;
+
+      // clone to avoid mutating original object before save
+      this.employee = { 
+        ...this.employeeData,
+        address: { ...this.employeeData.address }  // Deep clone nested object
+      };
+
+      // convert date to string for date input
+      this.employee.dateOfBirth =
+        this.formatDate(this.employee.dateOfBirth);
+
+      this.employee.joiningDate =
+        this.formatDate(this.employee.joiningDate);
+    } else {
+      this.employee = {
+        address: {
+          addressLine1: '',
+          addressLine2: '',
+          pinCode: ''
+        },
+        employeeImage: 'assets/images/default-user.png'
+      };
+    }
   }
 
-  submit( form: NgForm) {
-    console.log('Form Submitted', form.value);
-    form.resetForm();
-    this.employee = this.getEmptyEmployee(); // Reset the employee object
+  formatDate(date: any) {
+    if (!date) return '';
+    const d = new Date(date);
+    return d.toISOString().substring(0, 10);
+  }
+
+  submit(form: any) {
+
+    if (form.invalid) return;
+
+    const newEmployee: Employee = {
+      ...this.employee,
+      dateOfBirth: new Date(this.employee.dateOfBirth),
+      joiningDate: new Date(this.employee.joiningDate),
+      address: {
+        addressLine1: this.employee.address.addressLine1,
+        addressLine2: this.employee.address.addressLine2,
+        pinCode: this.employee.address.pinCode
+      }
+    };
+console.log('Adding Employee:', this.employee);
+    this.save.emit(newEmployee);
+
+    //form.resetForm();
+    
+  }
+
+  cancel() {
+    this.close.emit();
   }
 }
